bdd89e0f640e840602b1f54525cd978bc6eacd46
karan <professionalkaran300@gmail.com>
2025-09-29 19:05:56 +0530

    ADD BASIC FILES main,gin,env,mod

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Secirurity-to-pricacy app/project
+# Probekm-to-Idea-to-Solution-to-Security-to-Priacy app/project
 
 Author NAME of this repository is = Karan Chauhan
 
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
+)
+ func main(){
+	
+    r := gin.Default()
+	 c.Context
+	_ = r.Run(":8000")
+ }
